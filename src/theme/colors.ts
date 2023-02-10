bf01c7b96639151d4b3fde7703e16a37d414bb03
dualism <beachsidebearhi@gmail.com>
2023-02-10 12:48:10 +0700

    Added assets and app colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#FFD567',
+  label: '#FFF',
+  background: '#202020',
+  greyCell: '#383838',
+  greyTertiary: '#868686',
+  greySecondary: '#C0C0C0',
+};
+
+export default colors;
